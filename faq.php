e309cf9d721562899b5b7723627d6536297c1d60
YTrushkov <yuri.trushkov@gmail.com>
2017-04-10 18:57:43 +0000

    added 10v10 rules

--- faq.php
+++ faq.php
@@ -22,7 +22,7 @@ require('top.php');
         Any matches played inside another event or tournament (in 3v3, 7v7, or 10v10) may be reported here, as long as both teams are registered here and agree to the rules
         of that event. Informal scrims don't count unless explicitly agreed or the training room is tagged as a ladder match. The ladders
         are tier agnostic. It is up to the user to determine what tier(s) they wish to play in, either by explicit agreement or by training room choices.
-        The default rules are listed here, but teams may agree to any set of rules they wish, so long as they utilize a 3v3 or 7v7 player format.
+        The default rules are listed here, but teams may agree to any set of rules they wish, so long as they utilize the proper number of players for the respective ladder.
 </ol>
 
 <h3>Game setup</h3>
